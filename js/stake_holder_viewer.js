a3f2527956e53efbd71de926bba789f9ecc5f09f
Hoang Long <litpuvn@gmail.com>
2018-02-20 11:13:55 -0600

    support admin view

--- js/stake_holder_viewer.js
+++ js/stake_holder_viewer.js
@@ -1,20 +1,56 @@
 function StakeHolderViewer() {
 
-    this.wordCloud = new WordCloud('adminBoard');
+    // this.wordCloud = new WordCloud('adminBoard');
 
 }
 
 StakeHolderViewer.prototype = {
     constructor: StakeHolderViewer,
 
+    _getCtx: function (canvasElementId) {
+        return document.getElementById(canvasElementId).getContext('2d');
+
+    },
+
     _createAdminBoardVis: function () {
-        this.wordCloud.populateWordCloud();
+
+         // completely arbitrary data
+      var sampleData = {
+        labels: ['January', 'Feburary', 'March', 'April', 'May', 'June',
+                 'July', 'August', 'September','October', 'November', 'December' ],
+        datasets: [
+          {
+            label: 'Million',
+            data: [ 4, 4, 5.5, 4, 7, 12, 14, 9, 6, 5, 2, 1]
+          },
+          {
+            label: 'Wastewater',
+            data: [ 8, 2, 1, 0, 0, 0, 1, 3, 8, 12, 11, 10]
+          },
+          {
+            label: 'Conservation',
+            data: [0, 1, 2, 2, 3, 4, 3, 2, 2, 3, 0, 0]
+          },
+          {
+            label: 'Approve',
+            data: [0, 0, 0, 0, 0, 0, 2, 5, 9, 6, 5, 1]
+          },
+          {
+            label: 'Infrastructure',
+            data: [0, 3, 2, 3, 6, 3, 4, 1, 2, 4, 8, 2]
+          }
+        ]
+      };
+
+    var maxHeightChart = new Chart(this._getCtx('adminBoard'))
+        .Streamgraph(sampleData, {
+          responsive: true,
+          labelPlacementMethod: 'maxHeight'
+        });
+
     },
 
     _createAgChatConcerns: function () {
-      function ctx(elementId){
-        return document.getElementById(elementId).getContext('2d');
-      }
 
        // completely arbitrary data
       var sampleData = {
@@ -44,7 +80,7 @@ StakeHolderViewer.prototype = {
         ]
       };
 
-    var maxHeightChart = new Chart(ctx('agChatPlacementMaxHeightChart'))
+    var maxHeightChart = new Chart(this._getCtx('agChatPlacementMaxHeightChart'))
         .Streamgraph(sampleData, {
           responsive: true,
           labelPlacementMethod: 'maxHeight'
@@ -57,7 +93,7 @@ StakeHolderViewer.prototype = {
          vex.dialog.alert(
             {
                 message: 'Agriculture Chat',
-                className: 'water-admin-board',
+                className: 'ag-chat-board',
                 overlayClassName: 'water-admin-board-overlay',
                 showCloseButton: false,
                 escapeButtonCloses: true,
@@ -87,7 +123,7 @@ StakeHolderViewer.prototype = {
                 showCloseButton: false,
                 escapeButtonCloses: true,
                 overlayClosesOnClick: true,
-                input: '<div id="adminBoard"></div>',
+                input: '<canvas id="adminBoard"></canvas>',
                 buttons: [],
                 callback: function(data) {
                     if (!data) {
