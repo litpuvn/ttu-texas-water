470f454f8d3361dc55dbfa844ffde05edfe00cf8
Hoang Long <litpuvn@gmail.com>
2018-03-18 17:25:33 -0500

    add color to water

--- js/stake_holder_viewer.js
+++ js/stake_holder_viewer.js
@@ -45,7 +45,13 @@ StakeHolderViewer.prototype = {
     var maxHeightChart = new Chart(this._getCtx('adminBoard'))
         .Streamgraph(sampleData, {
           responsive: true,
-          labelPlacementMethod: 'maxHeight'
+          labelPlacementMethod: 'maxHeight',
+            colors: [
+                '#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94',
+                '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf'
+            ],
+            labelFontColor: 'black',
+            stroke: false
         });
 
     },
