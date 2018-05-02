4c03b07c54429a0b7a54b1619e54eaa2d0234d69
Cyrus67 <nickalvarez123@gmail.com>
2018-05-02 15:16:01 -0500

    Stopwords and improvements to word cloud

--- visualization/word_cloud.py
+++ visualization/word_cloud.py
@@ -1,73 +1,111 @@
 import os
 from os import path
-from wordcloud import WordCloud
+from dateutil.parser import parse
+from wordcloud import WordCloud, STOPWORDS
+
+
+#For parsing whether a line is a date.
+def isDate(string):
+    try:
+        parse(string)
+        return True
+    except ValueError:
+        return False
+
 
 d = path.dirname(path.realpath(__file__))
 dirPath = path.join(d, '../data/tweets/twdb_monthly')
 
-#Have 1 word cloud with no hashtags, just cut out the hashtag part.
-#Have another with only the hashtags
+#Have 1 word cloud with no hashtags.
+#Have another with only the hashtags.
 outputDataWords = []
 outputDataHashtags = []
 
 
 for filename in os.listdir(dirPath):
+
+    #if not filename == "2016-12.csv": continue
+    
     dataPath = path.join(dirPath, filename)
-    text = open(dataPath).read()
-    lines = text.split('\n')
-    del lines[0] #Remove the legend at the top of the file.
+    #text = open(dataPath).read()
+    with open(dataPath) as file:
+        text = file.read()
+        lines = text.split('\n')
+        del lines[0] #Remove the legend at the top of the file.
 
-    for line in lines:
-        #print(line)
-        lineParts = line.split(',')
-        if len(lineParts) < 5:
-            continue
-        #Remove the first, and last 3 elements.
-        #Probably a cleaner way to do this. This is faster than slicing.
-        del lineParts[0]
-        del lineParts[-1]
-        del lineParts[-1]
-        del lineParts[-1]
-        trimmedLine = ','.join(lineParts)
-
-        #Trim quotes if they're there.
-        if trimmedLine[0] == '"':
-            trimmedLine = trimmedLine[1:-1]
-
-        #Remove 'RT' for retweets
-        if trimmedLine.startswith('RT'):
-            trimmedLine = trimmedLine[3:]
-
-        #Word by Word processing
-        #processedWords = []
-        words = trimmedLine.split(' ')
-        for word in words:
-            #Ignore empty 'words'
-            if len(word) < 1:
+        for line in lines:
+            #print(line)
+            lineParts = line.split(',')
+            if len(lineParts) < 5: #Bad line
                 continue
-            #Fix & encoding
-            if word == '&amp;':
-                word = '&'
-            #Remove hyperlinks
-            httpIndex = word.find('http')
-            if httpIndex == 0: #Remove hyperlinks
-                continue #Just keep going. Don't add the word to the result.
-            elif httpIndex > 0: #Some hyperlinks typed into the word. Cut out link
-                word = word[0:httpIndex] #Cut out the link.
-            #Remove handles
-            if word[0] == '@':
-                continue
-            #Hashtags. Added to own list for separate word cloud
-            #Removed from normal word cloud.
-            if word[0] == '#':
-                #word = word[1:]
-                outputDataHashtags.append(word)
-                continue #Don't add hashtags to the word wordcloud
-            #After all the processing, if it got here, it's a good word.
-            #Append it to the list.
-            outputDataWords.append(word)
-            #processedWords.append(word)
-        #print(' '.join(processedWords) + '\n')
+            elif len(lineParts) == 5:
+                #Remove the first, and last 3 elements.
+                #Probably a cleaner way to do this. This is faster than slicing.
+                del lineParts[0] #Clear the ID
+                del lineParts[-1] #Clear the timestamp
+                del lineParts[-1] #Clear place
+                del lineParts[-1] #Clear coordinates
+            else:
+                del lineParts[0] #Clear the ID
+                del lineParts[-1] #Clear the timestamp
+                del lineParts[-1] #Clear place
+                del lineParts[-1] #Clear coordinates
+                indexesToDelete = []
+                tracingToEnd = False # Handles the crazy JSON object.
+                for i in range(0, len(lineParts)):
+                    if tracingToEnd: #For removing the JSON object, if it's there.
+                        if lineParts[i].endswith('}"'):
+                            tracingToEnd = False
+                        indexesToDelete.append(i)
+                        continue
+                    elif lineParts[i].startswith('None'): #Remove None line parts
+                        indexesToDelete.append(i)
+                    elif isDate(lineParts[i]): #Remove line parts that are just dates
+                        indexesToDelete.append(i)
+                    elif lineParts[i].startswith('"{'): #For removing the JSON object.
+                        tracingToEnd = True
+                        indexesToDelete.append(i)
+                for i in range(0, len(indexesToDelete)):
+                    del lineParts[indexesToDelete[i] - i]
+            trimmedLine = ','.join(lineParts)          
+            #Trim quotes if they're there.
+            if trimmedLine[0] == '"':
+                trimmedLine = trimmedLine[1:-1]
+
+            #Remove 'RT' for retweets
+            if trimmedLine.startswith('RT'):
+                trimmedLine = trimmedLine[3:]
+
+            #Word by Word processing
+            #processedWords = []
+            words = trimmedLine.split(' ')
+            for word in words:
+                #Ignore empty 'words'
+                if len(word) < 1:
+                    continue
+                #Fix & encoding
+                if word == '&amp;':
+                    word = '&'
+                #Remove hyperlinks
+                httpIndex = word.find('http')
+                if httpIndex == 0: #Remove hyperlinks
+                    continue #Just keep going. Don't add the word to the result.
+                elif httpIndex > 0: #Some hyperlinks typed into the word. Cut out link
+                    word = word[0:httpIndex] #Cut out the link.
+                #Remove handles
+                if word[0] == '@':
+                    continue
+                #Hashtags. Added to own list for separate word cloud
+                #Removed from normal word cloud.
+                if word[0] == '#':
+                    #word = word[1:]
+                    outputDataHashtags.append(word)
+                    continue #Don't add hashtags to the word wordcloud
+                #After all the processing, if it got here, it's a good word.
+                #Append it to the list.
+                outputDataWords.append(word)
+                #processedWords.append(word)
+            #print(' '.join(processedWords) + '\n')
 
 
 
@@ -86,22 +124,33 @@ print("Length of hashtag list: " + str(len(outputDataHashtags)))
 #Seems like it's cutting Texas and Texans into 'Texa' or something.
 #Will work on that later.
 
+#Stopwords
+stopWords = set(STOPWORDS)
+with open(path.join(d, 'stopwords.txt')) as stopWordsFile:
+    words = stopWordsFile.read().split('\n')
+    for word in words:
+        if not word.startswith(';'):
+            stopWords.add(word)
+    
+
+#stopWords.add("TEXA")
+
 #Plot and show the actual word cloud.
 import matplotlib.pyplot as plt
 
-#wordcloud = WordCloud(max_font_size=40).generate(' '.join(outputDataWords))
-wordcloud = WordCloud().generate(' '.join(outputDataWords))
-plt.figure()
+#wordcloud = WordCloud(background_color='white', max_font_size=40, stopwords=stopWords).generate(' '.join(outputDataWords))
+wordcloud = WordCloud(width=1280, height=720, background_color='white', stopwords=stopWords).generate(' '.join(outputDataWords))
+plt.figure(figsize=(14, 9))
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.title("Word WordCloud")
 
-#wordcloud = WordCloud(max_font_size=40).generate(' '.join(outputDataHashtags))
-wordcloud = WordCloud().generate(' '.join(outputDataHashtags))
-plt.figure()
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.title("Hashtag WordCloud")
+#wordcloud = WordCloud(background_color='white', max_font_size=40, stopwords=stopWords).generate(' '.join(outputDataHashtags))
+#wordcloud = WordCloud(background_color='white', stopwords=stopWords).generate(' '.join(outputDataHashtags))
+#plt.figure()
+#plt.imshow(wordcloud, interpolation='bilinear')
+#plt.axis("off")
+#plt.title("Hashtag WordCloud")
 plt.show()
 
 
