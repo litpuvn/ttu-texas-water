583af2e47677d1920acf33191bb480897dd7166f
Hoang Long <litpuvn@gmail.com>
2018-05-18 14:26:00 -0500

    add linear regression baseline

---
+++ baseline/linear_regression.py
@@ -0,0 +1,41 @@
+import csv
+from datetime import datetime, timedelta
+from sklearn import linear_model
+import numpy as np
+
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import math
+
+months = []
+water_levels = []
+
+# read data
+with open('carson-training.csv', newline='') as csvfile:
+    diagnosis_reader = csv.reader(csvfile, delimiter=',')
+    header = True
+    for row in diagnosis_reader:
+        if header:
+            header = False
+            continue
+        months.append(datetime.strptime(row[0], '%Y-%m'))
+        water_levels.append(float(row[1]))
+
+monts_num = range(0, len(months))
+monts_num = np.reshape(np.array(monts_num), (-1, 1))
+
+regr = linear_model.LinearRegression()
+regr.fit(monts_num, water_levels)
+
+
+trainPredict = regr.predict(monts_num)
+
+error = mean_squared_error(water_levels, trainPredict)
+print("Mean Squared Error: " + str(error))
+
+print("Root Mean Squared Error: " + str(math.sqrt(error)))
+
+error = r2_score(water_levels, trainPredict)
+print("R Squared Error: " + str(error))
+
+error = mean_absolute_error(water_levels, trainPredict)
+print("Mean Absolute Error: " + str(error))
